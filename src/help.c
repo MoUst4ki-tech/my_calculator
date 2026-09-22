bf778d2fb821cd8672be6a6a38fcc43f3ce8479c
MoUsT4Ki <noe1.marchal@epitech.eu>
2026-09-22 11:43:58 +0200

    Implement help function to display usage information when '-h' flag is provided

--- src/help.c
+++ src/help.c
@@ -7,5 +7,11 @@
 
 #include "my_calculator.h"
 
-
+int help(int argc, char **argv)
+{
+    if (argc == 2 && strcmp(argv[1], "-h") == 0) {
+        print_help();
+        return 1;
+    }
+    return 0;
 }
