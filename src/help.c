277349463c732f7bd23b75d5114aae83c64a614f
MoUsT4Ki <noe1.marchal@epitech.eu>
2026-09-22 11:43:25 +0200

    Update build workflow to use 'make fclean' for cleanup and remove unused help function

--- src/help.c
+++ src/help.c
@@ -7,11 +7,5 @@
 
 #include "my_calculator.h"
 
-int help(int argc, char **argv)
-{
-    if (argc == 2 && strcmp(argv[1], "-h") == 0) {
-        print_help();
-        return 1;
-    }
-    return 0;
+
 }
